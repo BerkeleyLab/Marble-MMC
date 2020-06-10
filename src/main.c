5d58bc26e934dd6a2563155cb64f79091089411c
Larry Doolittle <ldoolitt@recycle.lbl.gov>
2020-06-09 18:19:03 -0700

    Hack in simple REPL control over power supplies

--- src/main.c
+++ src/main.c
@@ -7,8 +7,42 @@
 // Setup UART strings
 const char demo_str[] = "Marble Mini v1 UART DEMO\r\n";
 const char lb_str[] = "Loopback... ESC to exit\r\n";
-const char menu_str[] = "\r\nMenu:\r\n0) Loopback\r\n1) FPGA IP/MAC update\r\n2) I2C monitor\r\n3) MDIO status\r\n";
+const char menu_str[] = "\r\n"
+	"Menu:\r\n"
+	"0) Loopback\r\n"
+	"1) FPGA IP/MAC update\r\n"
+	"2) I2C monitor\r\n"
+	"3) MDIO status\r\n"
+	"4) GPIO control\r\n";
 const char unk_str[] = "> Unknown option\r\n";
+const char gpio_str[] = "GPIO pins, caps for on, lower case for off\r\n"
+	"a) FMC power\r\n"
+	"b) EN_PSU_CH\r\n";
+
+// All of these are P1
+static void gpio_cmd(void)
+{
+   char rx_ch;
+   marble_UART_send(gpio_str, strlen(gpio_str));
+   while(marble_UART_recv(&rx_ch, 1) == 0);
+   bool on = ((rx_ch >> 5) & 1) == 0;
+   if (on) marble_UART_send("ON\r\n", 4);
+   else marble_UART_send("OFF\r\n", 5);
+   switch (rx_ch) {
+      case 'a':
+      case 'A':
+         marble_FMC_pwr(on);
+         break;
+      case 'b':
+      case 'B':
+         Chip_GPIO_WriteDirBit(LPC_GPIO, 1, 31, true);
+         Chip_GPIO_WritePortBit(LPC_GPIO, 1, 31, on);
+         break;
+      default:
+         marble_UART_send(unk_str, strlen(unk_str));
+         break;
+      }
+}
 
 int main (void) {
 
@@ -53,6 +87,9 @@ int main (void) {
             I2C_PM_probe();
             break;
          case '3':
+            break;
+         case '4':
+            gpio_cmd();
             break;
          default:
             marble_UART_send(unk_str, strlen(unk_str));
