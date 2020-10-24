95e3293cec0e975a73713006470552e55fde983a
Vamsi Vytla <vkvytla@lbl.gov>
2020-10-23 20:05:44 -0700

    Src/program_xrp.py: Successful communicates with the MMC program via UART

--- Src/program_xrp.py
+++ Src/program_xrp.py
@@ -1,16 +1,26 @@
+#!/usr/bin/env python
+
+import time
+
 import serial
 
-
-def program(hex_file, serial_port, dtr=1):
+def program(hex_file, serial_port):
     '''
     Goal here is to open the serial port to MMC on Marble
     And exectute a sequence of steps to program the XR chip
 
     dtr is here to force a reboot of the MMC
     '''
-    with serial.Serial(serial_port, 115200) as ser:
-        print(ser.dtr)
-        # ser.write('g')
+    with serial.Serial(serial_port, 115200, timeout=1) as ser:
+        ser.rts, ser.dtr = False, False
+        ser.write(b'?')
+        while True:
+            x = ser.readline().decode('utf-8')
+            if x: print(x, end='')
+            else: break
+        ser.write(b'3')
+        print(ser.readline().decode('utf-8'))
+        return
         for line in open(hex_file, 'r').readlines():
             line = line.strip()
             if line.startswith(':') and len(line) > 3:
@@ -22,7 +32,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Program XR via MMC")
     parser.add_argument("-u", "--uart-device",
                         help="MMC UART device")
-    parser.add_argument("-h", "--hex-file", default="Marble_runtime_3A.hex",
+    parser.add_argument("-hf", "--hex-file", default="Marble_runtime_3A.hex",
                         help="Intex hex recort program file")
     args = parser.parse_args()
     program(args.hex_file, args.uart_device)
