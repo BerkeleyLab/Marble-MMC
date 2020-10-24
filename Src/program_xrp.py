a182fcff7abb6741045224c782c4d88fc2fea47c
Vamsi Vytla <VKVytla@lbl.gov>
2020-10-23 20:41:59 -0700

    Src/program_xrp.py: Gets OKs from MMC while loading the hexfile. Still unsure how to send that program to XR

--- Src/program_xrp.py
+++ Src/program_xrp.py
@@ -4,24 +4,32 @@ import time
 
 import serial
 
+def read_and_print_till_empty(ser):
+    while True:
+        x = ser.readline().decode('utf-8')
+        if x: print(x, end='')
+        else: break
+    print()
+
 def program(hex_file, serial_port):
     '''
     Goal here is to open the serial port to MMC on Marble
     And exectute a sequence of steps to program the XR chip
     '''
     with serial.Serial(serial_port, 115200, timeout=1) as ser:
-        ser.write(b'?')
-        while True:
-            x = ser.readline().decode('utf-8')
-            if x: print(x, end='')
-            else: break
-        ser.write(b'3')
-        print(ser.readline().decode('utf-8'))
-        return
+        print('?'), ser.write(b'?')
+        read_and_print_till_empty(ser)
+        print('3'), ser.write(b'3')
+        read_and_print_till_empty(ser)
+        print('h'), ser.write(b'h')
+        read_and_print_till_empty(ser)
+
         for line in open(hex_file, 'r').readlines():
             line = line.strip()
             if line.startswith(':') and len(line) > 3:
-                ser.write(SOME_FORM_OF_LINE)
+                ser.write(bytes(line, 'ascii'))
+                read_and_print_till_empty(ser)
+
 
 
 if __name__ == "__main__":
