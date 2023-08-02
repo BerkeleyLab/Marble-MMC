a9b7b6f6f51d7fe9d45f4d15e4a223746db28762
sdmurthy <sdmurthy@lbl.gov>
2023-08-01 18:26:18 -0700

    Add initial function to read telemetry data from LTM4673 power management chip for verions>1.3. Tested on hardware

--- src/i2c_pm.c
+++ src/i2c_pm.c
@@ -298,6 +298,64 @@ void I2C_PM_probe(void)
    return;
 }
 
+/* LTM4673, new power management chip
+ * valid only for builds > v1.3
+ *
+ */
+int ltm_read_telem(uint8_t dev)
+{
+  if (marble_get_board_id() < Marble_v1_3) {
+    printf("LTM4673 not present; bypassed.\n");
+    return 0;
+  }
+   struct {int b; const char *m;} r_table[] = {
+      // see page 105
+      {0x88, "READ_VIN"},
+      {0x89, "READ_IIN"},
+      {0x97, "READ_PIN"},
+      {0x8B, "READ_VOUT"},
+      {0x8C, "READ_IOUT"},
+      {0x8D, "READ_TEMPERATURE_1"},
+      {0x8E, "READ_TEMPERATURE_2"},
+      {0x96, "READ_POUT"},
+      {0xBB, "MFR_READ_IOUT"},
+      {0xC4, "MFR_IIN_PEAK"},
+      {0xC5, "MFR_IIN_MIN"},
+      {0xC6, "MFR_PIN_PEAK"},
+      {0xC7, "MFR_PIN_MIN"},
+      {0xFA, "MFR_IOUT_SENSE_VOLTAGE"},
+      {0xDE, "MFR_VIN_PEAK"},
+      {0xDD, "MFR_VOUT_PEAK"},
+      {0xD7, "MFR_IOUT_PEAK"},
+      {0xDF, "MFR_TEMPERATURE_1_PEAK"},
+      {0xFC, "MFR_VIN_MIN"},
+      {0xFB, "MFR_VOUT_MIN"},
+      {0xD8, "MFR_IOUT_MIN"},
+      {0xFD, "MFR_TEMPERATURE_1_MIN"}};
+   printf("LTM4673 Telemetry register dump:\n");
+   for (unsigned jx = 0; jx < 4; jx++) {
+      // start selecting channel/page 0 until you finish reading
+      // telemetry data for all 4 channels
+      uint8_t page = 0x00 + jx;
+      marble_I2C_cmdsend(I2C_PM, dev, 0x00, &page, 1);
+      int rc1 = marble_I2C_cmdrecv(I2C_PM, dev, 0x00, &page, 1);
+      printf("> Read page/channel: %x: cmd: %2.2x\r\n", page, rc1);
+      const unsigned tlen = sizeof(r_table)/sizeof(r_table[0]);
+      for (unsigned ix=0; ix<tlen; ix++) {
+          uint16_t i2c_dat[4];
+          int regno = r_table[ix].b;
+          int rc = marble_I2C_cmdrecv(I2C_PM, dev, regno, i2c_dat, 2);
+          if (rc == HAL_OK) {
+              // TODO: convert them to physical units
+              printf("r[%2.2x] = 0x%4.4x = %5d = 2(%s)\r\n", regno, i2c_dat[0], i2c_dat[0], r_table[ix].m);
+          } else {
+              printf("r[%2.2x]    unread          (%s)\r\n", regno, r_table[ix].m);
+          }
+      }
+   }
+}
+
+
 /* XPR7724 is special
  * Seems that one-byte Std Commands documented in ANP-38 apply to
  * commands < 0x4F, but there are also two-byte commands (called addresses)
--- src/main.c
+++ src/main.c
@@ -15,6 +15,9 @@ static void mgtclk_xpoint_en(void)
 {
    if (xrp_ch_status(XRP7724, 1)) { // CH1: 3.3V
       adn4600_init();
+   } else if (ltm_read_telem(LTM4673)) {
+      printf("Using LTM4673 and adn4600_init\r\n");
+      adn4600_init();
    } else {
       printf("Skipping adn4600_init\r\n");
    }
