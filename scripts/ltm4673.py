106c7a0ca6d244761d6cf7a83640ce130c1f9d09
Keith Penney <kpenney@lbl.gov>
2023-10-13 00:28:20 -0700

    Including program from LTC Windows programming GUI

--- scripts/ltm4673.py
+++ scripts/ltm4673.py
@@ -202,6 +202,9 @@ commands = {
     "MFR_TEMPERATURE_1_MIN":         (0xfd, MODE_WORD),
 }
 
+for name, arg in commands.items():
+    globals()[name] = arg[0]
+
 def print_commands_c():
     for name, arg in commands.items():
         addr, mode = arg
@@ -210,7 +213,7 @@ def print_commands_c():
 
 def write_byte(cmd, val):
     #   WRITE_BYTE:         | cmd + wr  | command code | data byte |
-    args = vet_args(cmd, val)
+    args = vet_args_byte(cmd, val)
     if args is None:
         return False
     else:
@@ -224,7 +227,7 @@ def write_byte(cmd, val):
 
 def write_word(cmd, val):
     #   WRITE_WORD:         | addr + wr  | command code | data word low | data word high |
-    args = vet_args(cmd, val)
+    args = vet_args_word(cmd, val)
     if args is None:
         return False
     else:
@@ -232,13 +235,13 @@ def write_word(cmd, val):
     if not hasattr(val, '__len__'):
         # val should be list of bytes (lsb to msb)
         val = (val & 0xff, (val >> 8) & 0xff)
-    msg = (addr_dev + WR, cmd, val)
+    msg = (addr_dev + WR, cmd, val[0], val[1])
     return (msg,)
 
 def send_byte(cmd, val):
     # val ignored
     #   SEND_BYTE:          | addr + wr  | command code |
-    args = vet_args(cmd, 0)
+    args = vet_args_byte(cmd, 0)
     if args is None:
         return False
     else:
@@ -248,7 +251,7 @@ def send_byte(cmd, val):
 
 def write_byte_pec(cmd, val):
     #   WRITE_BYTE w/ PEC:  | addr + wr  | command code | data byte | PEC |
-    args = vet_args(cmd, val)
+    args = vet_args_byte(cmd, val)
     if args is None:
         return False
     else:
@@ -263,7 +266,7 @@ def write_byte_pec(cmd, val):
 
 def write_word_pec(cmd, val):
     #   WRITE_WORD w/ PEC:  | addr + wr  | command code | data word low | data word high | PEC |
-    args = vet_args(cmd, val)
+    args = vet_args_word(cmd, val)
     if args is None:
         return False
     else:
@@ -271,14 +274,14 @@ def write_word_pec(cmd, val):
     if not hasattr(val, '__len__'):
         # val should be list of bytes (lsb to msb)
         val = (val & 0xff, (val >> 8) & 0xff)
-    msg = [addr_dev + WR, cmd, val]
+    msg = [addr_dev + WR, cmd, val[0], val[1]]
     pec = calc_pec(*msg)
     return (msg + [pec],)
 
 def send_byte_pec(cmd, val):
     # val ignored
     #   SEND_BYTE w/ PEC:   | addr + wr  | command code | PEC |
-    args = vet_args(cmd, 0)
+    args = vet_args_byte(cmd, 0)
     if args is None:
         return False
     else:
@@ -290,7 +293,7 @@ def send_byte_pec(cmd, val):
 def read_byte(cmd, val):
     # val ignored
     #   READ_BYTE:          | addr + wr  | command code ! addr + rd | data byte |
-    args = vet_args(cmd, 0)
+    args = vet_args_byte(cmd, 0)
     if args is None:
         return False
     else:
@@ -302,7 +305,7 @@ def read_byte(cmd, val):
 def read_word(cmd, val):
     # val ignored
     #   READ_WORD:          | addr + wr  | command code ! addr + rd | data word high | data word low |
-    args = vet_args(cmd, 0)
+    args = vet_args_word(cmd, 0)
     if args is None:
         return False
     else:
@@ -318,7 +321,7 @@ def read_byte_pec(cmd, val):
 def read_word_pec(cmd, val):
     # val ignored
     #   READ_WORD w/ PEC:   | addr + wr  | command code ! addr + rd | data word high | data word low | PEC |
-    args = vet_args(cmd, 0)
+    args = vet_args_word(cmd, 0)
     if args is None:
         return False
     else:
@@ -330,7 +333,7 @@ def read_word_pec(cmd, val):
 def read_block(cmd, val):
     # val ignored
     #   READ_BLOCK:         | addr + wr  | command code ! addr + rd | byte count N | byte 0 |...| byte N-1 |
-    args = vet_args(cmd, 0)
+    args = vet_args_byte(cmd, 0)
     if args is None:
         return False
     else:
@@ -369,6 +372,7 @@ def get_xact(cmd, val, pec=False):
     if params is None:
         return None
     name, cmd, mode = params
+    #print(f"name = {name}, cmd = {cmd}, mode = {mode}")
     if mode == MODE_SEND:
         if pec:
             return send_byte_pec(cmd, 0)
@@ -407,7 +411,11 @@ def get_xact(cmd, val, pec=False):
     # Unknown mode
     return None
 
-def vet_args(cmd, val):
+def vet_args(cmd, val, mode_word = False):
+    if mode_word:
+        max_val = 0xffff
+    else:
+        max_val = 0xff
     try:
         cmd = int(cmd)
     except ValueError:
@@ -417,7 +425,7 @@ def vet_args(cmd, val):
     cmd = cmd & 0xff
     # val can be int or list of ints (LSB-first)
     try:
-        val = int(val) & 0xff
+        val = int(val) & max_val
     except ValueError:
         pass
     if hasattr(val, '__len__'):
@@ -431,6 +439,12 @@ def vet_args(cmd, val):
             return None
     return cmd, val
 
+def vet_args_byte(cmd, val):
+    return vet_args(cmd, val, mode_word=False)
+
+def vet_args_word(cmd, val):
+    return vet_args(cmd, val, mode_word=True)
+
 # =============================== PROGRAM API ================================
 # Each returns nested list of bytes
 #   ((addr_byte + rnw, cmd_code, ...), (addr_byte + rnw, cmd_code, ...), ...)
@@ -439,7 +453,7 @@ def vet_args(cmd, val):
 # If a msg byte is a list (empty), it means an unknown number of bytes should be
 # read (determined by the N value returned by the first read byte).
 
-def write(cmd, val):
+def write(cmd, val, sleep=True):
     return get_xact(cmd, val, pec=False)
 
 def read(cmd):
@@ -457,7 +471,7 @@ def translate_xact_mmc(xact):
             elif hasattr(mbyte, '__len__'):
                 line.append(MMC_READ_BLOCK)
             else:
-                line.append("0x{mbyte:02x}")
+                line.append(f"0x{mbyte:02x}")
         first = False
     return line
 
@@ -496,5 +510,238 @@ MMC_READ_BLOCK      = '*'
 #   0xHH: Use hex value 0xHH as the next transaction byte
 #   DDD : Use decimal value DDD as the next transaction byte
 
+def trial():
+    xacts = []
+    xacts.append(write(VIN_ON,0xCA40))
+    lines = translate_mmc(xacts)
+    print(lines)
+
+def program():
+    """Program derived from LTC PMBus Project Text File Version:1.1"""
+    xacts = []
+    # Select PAGE 0xff
+    xacts.append(write(PAGE,0xff))
+
+    xacts.append(write(WRITE_PROTECT,0x00))
+    xacts.append(write(VIN_ON,0xCA40))
+    xacts.append(write(VIN_OFF,0xCA33))
+    xacts.append(write(VIN_OV_FAULT_LIMIT,0xD3C0))
+    xacts.append(write(VIN_OV_FAULT_RESPONSE,0x80))
+    xacts.append(write(VIN_OV_WARN_LIMIT,0xD380))
+    xacts.append(write(VIN_UV_WARN_LIMIT,0x8000))
+    xacts.append(write(VIN_UV_FAULT_LIMIT,0x8000))
+    xacts.append(write(VIN_UV_FAULT_RESPONSE,0x00))
+    xacts.append(write(USER_DATA_00,0x0000))
+    xacts.append(write(USER_DATA_02,0x0000))
+    xacts.append(write(USER_DATA_04,0x0000))
+    xacts.append(write(MFR_EIN_CONFIG,0x00))
+    xacts.append(write(MFR_IIN_CAL_GAIN_TC,0x0000))
+    xacts.append(write(MFR_CONFIG_ALL_LTM4673,0x0F73))
+    xacts.append(write(MFR_PWRGD_EN,0x0000))
+    xacts.append(write(MFR_FAULTB0_RESPONSE,0x00))
+    xacts.append(write(MFR_FAULTB1_RESPONSE,0x00))
+    xacts.append(write(MFR_CONFIG2_LTM4673,0x00))
+    xacts.append(write(MFR_CONFIG3_LTM4673,0x00))
+    xacts.append(write(MFR_RETRY_DELAY,0xF320))
+    xacts.append(write(MFR_RESTART_DELAY,0xFB20))
+    xacts.append(write(MFR_POWERGOOD_ASSERTION_DELAY,0xEB20))
+    xacts.append(write(MFR_WATCHDOG_T_FIRST,0x8000))
+    xacts.append(write(MFR_WATCHDOG_T,0x8000))
+    xacts.append(write(MFR_PAGE_FF_MASK,0x0F))
+    xacts.append(write(MFR_I2C_BASE_ADDRESS,0x5C))
+    xacts.append(write(MFR_IIN_CAL_GAIN,0xCA80))
+    xacts.append(write(MFR_RETRY_COUNT,0x07))
+
+    # Select PAGE 0
+    xacts.append(write(PAGE,0x00))
+
+    xacts.append(write(OPERATION,0x80))
+    xacts.append(write(ON_OFF_CONFIG,0x1E))
+    xacts.append(write(VOUT_COMMAND,0x2000))
+    xacts.append(write(VOUT_MAX,0x8000))
+    xacts.append(write(VOUT_MARGIN_HIGH,0x219A))
+    xacts.append(write(VOUT_MARGIN_LOW,0x1E66))
+    xacts.append(write(VOUT_OV_FAULT_LIMIT,0x2333))
+    xacts.append(write(VOUT_OV_FAULT_RESPONSE,0x80))
+    xacts.append(write(VOUT_OV_WARN_LIMIT,0x223D))
+    xacts.append(write(VOUT_UV_WARN_LIMIT,0x1DC3))
+    xacts.append(write(VOUT_UV_FAULT_LIMIT,0x1CCD))
+    xacts.append(write(VOUT_UV_FAULT_RESPONSE,0x7F))
+    xacts.append(write(IOUT_OC_FAULT_LIMIT,0xDA20))
+    xacts.append(write(IOUT_OC_FAULT_RESPONSE,0x00))
+    xacts.append(write(IOUT_OC_WARN_LIMIT,0xD340))
+    xacts.append(write(IOUT_UC_FAULT_LIMIT,0xC500))
+    xacts.append(write(IOUT_UC_FAULT_RESPONSE,0x00))
+    xacts.append(write(OT_FAULT_LIMIT,0xF200))
+    xacts.append(write(OT_FAULT_RESPONSE,0xB8))
+    xacts.append(write(OT_WARN_LIMIT,0xEBE8))
+    xacts.append(write(UT_WARN_LIMIT,0xDD80))
+    xacts.append(write(UT_FAULT_LIMIT,0xE530))
+    xacts.append(write(UT_FAULT_RESPONSE,0xB8))
+    xacts.append(write(POWER_GOOD_ON,0x1EB8))
+    xacts.append(write(POWER_GOOD_OFF,0x1E14))
+    xacts.append(write(TON_DELAY,0xBA00))
+    xacts.append(write(TON_RISE,0xE320))
+    xacts.append(write(TON_MAX_FAULT_LIMIT,0xF258))
+    xacts.append(write(TON_MAX_FAULT_RESPONSE,0xB8))
+    xacts.append(write(TOFF_DELAY,0xBA00))
+    xacts.append(write(USER_DATA_01,0x0000))
+    xacts.append(write(USER_DATA_03,0x0000))
+    xacts.append(write(MFR_IOUT_CAL_GAIN_TAU_INV,0x8000))
+    xacts.append(write(MFR_IOUT_CAL_GAIN_THETA,0x8000))
+    xacts.append(write(MFR_CONFIG_LTM4673,0x0088))
+    xacts.append(write(MFR_FAULTB0_PROPAGATE,0x00))
+    xacts.append(write(MFR_FAULTB1_PROPAGATE,0x00))
+    xacts.append(write(MFR_DAC,0x01FF))
+    xacts.append(write(MFR_VOUT_DISCHARGE_THRESHOLD,0xC200))
+    xacts.append(write(MFR_IOUT_CAL_GAIN_TC,0x0000))
+    xacts.append(write(MFR_TEMP_1_GAIN,0x4000))
+    xacts.append(write(MFR_TEMP_1_OFFSET,0x8000))
+
+    # Select PAGE 1
+    xacts.append(write(PAGE,0x01))
+
+    xacts.append(write(OPERATION,0x80))
+    xacts.append(write(ON_OFF_CONFIG,0x1E))
+    xacts.append(write(VOUT_COMMAND,0x399A))
+    xacts.append(write(VOUT_MAX,0xFFFF))
+    xacts.append(write(VOUT_MARGIN_HIGH,0x3C7B))
+    xacts.append(write(VOUT_MARGIN_LOW,0x36B9))
+    xacts.append(write(VOUT_OV_FAULT_LIMIT,0x3F5D))
+    xacts.append(write(VOUT_OV_FAULT_RESPONSE,0x80))
+    xacts.append(write(VOUT_OV_WARN_LIMIT,0x3DEC))
+    xacts.append(write(VOUT_UV_WARN_LIMIT,0x3548))
+    xacts.append(write(VOUT_UV_FAULT_LIMIT,0x33D7))
+    xacts.append(write(VOUT_UV_FAULT_RESPONSE,0x7F))
+    xacts.append(write(IOUT_OC_FAULT_LIMIT,0xD200))
+    xacts.append(write(IOUT_OC_FAULT_RESPONSE,0x00))
+    xacts.append(write(IOUT_OC_WARN_LIMIT,0xCB00))
+    xacts.append(write(IOUT_UC_FAULT_LIMIT,0xBD00))
+    xacts.append(write(IOUT_UC_FAULT_RESPONSE,0x00))
+    xacts.append(write(OT_FAULT_LIMIT,0xF200))
+    xacts.append(write(OT_FAULT_RESPONSE,0xB8))
+    xacts.append(write(OT_WARN_LIMIT,0xEBE8))
+    xacts.append(write(UT_WARN_LIMIT,0xDD80))
+    xacts.append(write(UT_FAULT_LIMIT,0xE530))
+    xacts.append(write(UT_FAULT_RESPONSE,0xB8))
+    xacts.append(write(POWER_GOOD_ON,0x372F))
+    xacts.append(write(POWER_GOOD_OFF,0x3643))
+    xacts.append(write(TON_DELAY,0xEB20))
+    xacts.append(write(TON_RISE,0xE320))
+    xacts.append(write(TON_MAX_FAULT_LIMIT,0xF258))
+    xacts.append(write(TON_MAX_FAULT_RESPONSE,0xB8))
+    xacts.append(write(TOFF_DELAY,0xBA00))
+    xacts.append(write(USER_DATA_01,0x0000))
+    xacts.append(write(USER_DATA_03,0x0000))
+    xacts.append(write(MFR_IOUT_CAL_GAIN_TAU_INV,0x8000))
+    xacts.append(write(MFR_IOUT_CAL_GAIN_THETA,0x8000))
+    xacts.append(write(MFR_CONFIG_LTM4673,0x1088))
+    xacts.append(write(MFR_FAULTB0_PROPAGATE,0x00))
+    xacts.append(write(MFR_FAULTB1_PROPAGATE,0x00))
+    xacts.append(write(MFR_DAC,0x0245))
+    xacts.append(write(MFR_VOUT_DISCHARGE_THRESHOLD,0xC200))
+    xacts.append(write(MFR_IOUT_CAL_GAIN_TC,0x0000))
+    xacts.append(write(MFR_TEMP_1_GAIN,0x4000))
+    xacts.append(write(MFR_TEMP_1_OFFSET,0x8000))
+
+    # Select PAGE 2
+    xacts.append(write(PAGE,0x02))
+
+    xacts.append(write(OPERATION,0x80))
+    xacts.append(write(ON_OFF_CONFIG,0x1E))
+    xacts.append(write(VOUT_COMMAND,0x5000))
+    xacts.append(write(VOUT_MAX,0xFFFF))
+    xacts.append(write(VOUT_MARGIN_HIGH,0x5400))
+    xacts.append(write(VOUT_MARGIN_LOW,0x4C00))
+    xacts.append(write(VOUT_OV_FAULT_LIMIT,0x5800))
+    xacts.append(write(VOUT_OV_FAULT_RESPONSE,0x80))
+    xacts.append(write(VOUT_OV_WARN_LIMIT,0x563D))
+    xacts.append(write(VOUT_UV_WARN_LIMIT,0x4A3D))
+    xacts.append(write(VOUT_UV_FAULT_LIMIT,0x4800))
+    xacts.append(write(VOUT_UV_FAULT_RESPONSE,0x7F))
+    xacts.append(write(IOUT_OC_FAULT_LIMIT,0xD200))
+    xacts.append(write(IOUT_OC_FAULT_RESPONSE,0x00))
+    xacts.append(write(IOUT_OC_WARN_LIMIT,0xCB00))
+    xacts.append(write(IOUT_UC_FAULT_LIMIT,0xBD00))
+    xacts.append(write(IOUT_UC_FAULT_RESPONSE,0x00))
+    xacts.append(write(OT_FAULT_LIMIT,0xF200))
+    xacts.append(write(OT_FAULT_RESPONSE,0xB8))
+    xacts.append(write(OT_WARN_LIMIT,0xEBE8))
+    xacts.append(write(UT_WARN_LIMIT,0xDD80))
+    xacts.append(write(UT_FAULT_LIMIT,0xE530))
+    xacts.append(write(UT_FAULT_RESPONSE,0xB8))
+    xacts.append(write(POWER_GOOD_ON,0x4CE1))
+    xacts.append(write(POWER_GOOD_OFF,0x4B1F))
+    xacts.append(write(TON_DELAY,0xF320))
+    xacts.append(write(TON_RISE,0xE320))
+    xacts.append(write(TON_MAX_FAULT_LIMIT,0xF258))
+    xacts.append(write(TON_MAX_FAULT_RESPONSE,0xB8))
+    xacts.append(write(TOFF_DELAY,0xBA00))
+    xacts.append(write(USER_DATA_01,0xB48F))
+    xacts.append(write(USER_DATA_03,0x0000))
+    xacts.append(write(MFR_IOUT_CAL_GAIN_TAU_INV,0x8000))
+    xacts.append(write(MFR_IOUT_CAL_GAIN_THETA,0x8000))
+    xacts.append(write(MFR_CONFIG_LTM4673,0x2088))
+    xacts.append(write(MFR_FAULTB0_PROPAGATE,0x00))
+    xacts.append(write(MFR_FAULTB1_PROPAGATE,0x00))
+    xacts.append(write(MFR_DAC,0x0218))
+    xacts.append(write(MFR_VOUT_DISCHARGE_THRESHOLD,0xC200))
+    xacts.append(write(MFR_IOUT_CAL_GAIN_TC,0x0000))
+    xacts.append(write(MFR_TEMP_1_GAIN,0x4000))
+    xacts.append(write(MFR_TEMP_1_OFFSET,0x8000))
+
+    # Select PAGE 3
+    xacts.append(write(PAGE,0x03))
+
+    xacts.append(write(OPERATION,0x80))
+    xacts.append(write(ON_OFF_CONFIG,0x1E))
+    xacts.append(write(VOUT_COMMAND,0x699A))
+    xacts.append(write(VOUT_MAX,0xFFFF))
+    xacts.append(write(VOUT_MARGIN_HIGH,0x6EE2))
+    xacts.append(write(VOUT_MARGIN_LOW,0x6452))
+    xacts.append(write(VOUT_OV_FAULT_LIMIT,0x7429))
+    xacts.append(write(VOUT_OV_FAULT_RESPONSE,0x80))
+    xacts.append(write(VOUT_OV_WARN_LIMIT,0x71D7))
+    xacts.append(write(VOUT_UV_WARN_LIMIT,0x615D))
+    xacts.append(write(VOUT_UV_FAULT_LIMIT,0x5F0B))
+    xacts.append(write(VOUT_UV_FAULT_RESPONSE,0x7F))
+    xacts.append(write(IOUT_OC_FAULT_LIMIT,0xDA20))
+    xacts.append(write(IOUT_OC_FAULT_RESPONSE,0x00))
+    xacts.append(write(IOUT_OC_WARN_LIMIT,0xD340))
+    xacts.append(write(IOUT_UC_FAULT_LIMIT,0xC500))
+    xacts.append(write(IOUT_UC_FAULT_RESPONSE,0x00))
+    xacts.append(write(OT_FAULT_LIMIT,0xF200))
+    xacts.append(write(OT_FAULT_RESPONSE,0xB8))
+    xacts.append(write(OT_WARN_LIMIT,0xEBE8))
+    xacts.append(write(UT_WARN_LIMIT,0xDD80))
+    xacts.append(write(UT_FAULT_LIMIT,0xE530))
+    xacts.append(write(UT_FAULT_RESPONSE,0xB8))
+    xacts.append(write(POWER_GOOD_ON,0x64F5))
+    xacts.append(write(POWER_GOOD_OFF,0x63B0))
+    xacts.append(write(TON_DELAY,0xFA58))
+    xacts.append(write(TON_RISE,0xE320))
+    xacts.append(write(TON_MAX_FAULT_LIMIT,0xF258))
+    xacts.append(write(TON_MAX_FAULT_RESPONSE,0xB8))
+    xacts.append(write(TOFF_DELAY,0xBA00))
+    xacts.append(write(USER_DATA_01,0x3322))
+    xacts.append(write(USER_DATA_03,0x0000))
+    xacts.append(write(MFR_IOUT_CAL_GAIN_TAU_INV,0x8000))
+    xacts.append(write(MFR_IOUT_CAL_GAIN_THETA,0x8000))
+    xacts.append(write(MFR_CONFIG_LTM4673,0x3088))
+    xacts.append(write(MFR_FAULTB0_PROPAGATE,0x00))
+    xacts.append(write(MFR_FAULTB1_PROPAGATE,0x00))
+    xacts.append(write(MFR_DAC,0x01CB))
+    xacts.append(write(MFR_VOUT_DISCHARGE_THRESHOLD,0xC200))
+    xacts.append(write(MFR_IOUT_CAL_GAIN_TC,0x0000))
+    xacts.append(write(MFR_TEMP_1_GAIN,0x4000))
+    xacts.append(write(MFR_TEMP_1_OFFSET,0x8000))
+
+    lines = translate_mmc(xacts)
+    for line in lines:
+        print(line)
+
 if __name__ == "__main__":
-    print_commands_c()
+    #print_commands_c()
+    #print(globals())
+    #trial()
+    program()
