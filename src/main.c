0f5cd1101b26a5ce7283548acac4fde0f7e3443b
sdmurthy <sdmurthy@lbl.gov>
2023-12-07 14:19:22 -0800

    main.c: fix typo, change ltm_ch_status to ltm4673_cht_status

--- src/main.c
+++ src/main.c
@@ -17,7 +17,7 @@ static void mgtclk_xpoint_en(void)
 {
    if ((marble_get_pcb_rev() < Marble_v1_4) & xrp_ch_status(XRP7724, 1)) { // CH1: 3.3V
       adn4600_init();
-   } else if ((marble_get_pcb_rev() > Marble_v1_3) & ltm_ch_status(LTM4673)) {
+   } else if ((marble_get_pcb_rev() > Marble_v1_3) & ltm4673_ch_status(LTM4673)) {
       printf("Using LTM4673 and adn4600_init\r\n");
       adn4600_init();
    } else {
