dd2a76f6f14cb888e8d44baec0617f7c04fef82a
stijnwielandt <stijnwielandt@lbl.gov>
2025-12-10 19:00:30 -0800

    fixed ERROR_ETH_MDIO_ID error during startup - relaxed HSE timout to 3000ms and added MX_ETH_MDIO_Init delay for clock stabilization

--- board_support/marble_v2/marble_board.c
+++ board_support/marble_v2/marble_board.c
@@ -1239,13 +1239,13 @@ uint32_t marble_init(void)
 {
   HAL_Init();
   SystemClock_Config_HSI();
-
   marble_UART_init();
   MX_GPIO_Init();
   marble_GPIOint_init();
   marble_read_pcb_rev();
   marble_get_SN();
   marble_PSU_pwr(true);
+  printf("        PSU and clocks initialized (%ld)\r\n", marble_get_tick());
   MX_ETH_MDIO_Init();
 
   printf("+ Init I2C FPGA interface...\r\n");
@@ -1373,7 +1373,7 @@ static void show_marble_SN(void) {
 
 static void SystemClock_Config(void)
 {
-
+  // HSE_STARTUP_TIMEOUT defined in stm32f2xx_hal_conf.h defines timeout for HSE start-up
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
   uint32_t FLatency;
@@ -1488,7 +1488,7 @@ static void MX_ETH_MDIO_Init(void)
 
     __HAL_ETH_RESET_HANDLE_STATE(&heth);
     HAL_ETH_MspInit(&heth);
-    marble_SLEEP_ms(10);
+    marble_SLEEP_ms(100);
     uint32_t id1, id2;
     if (HAL_ETH_ReadPHYRegister(&heth, MDIO_PHY_REG_PHY_ID_1, &id1) == HAL_OK &&
         HAL_ETH_ReadPHYRegister(&heth, MDIO_PHY_REG_PHY_ID_2, &id2) == HAL_OK)
